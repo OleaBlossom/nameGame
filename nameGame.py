578a75baa745c20097afebcfbaac88037508d7d4
OleaBlossom <o.corcoran@gmail.com>
2019-07-08 22:47:26 +0200

    modify json file structure, refactor code to use pathlib instead of os

--- nameGame.py
+++ nameGame.py
@@ -1,16 +1,16 @@
-import os
+from pathlib import Path as path
 import json
 import random
 
 # create two lists of potential games: the file path and the user-facing title
 game_paths = []
 game_titles = []
-base_path = "/Users/oleablossom/Documents/Python/hello/"
-for i in os.listdir(base_path):
-    if i.endswith(".json"):
-        game_paths.append("%s" % i)
-        with open(i, "r") as f:
-            game_titles.append(json.loads(f.read()).get("title"))
+p = path(".")
+for i in p.glob("*.json"):
+    game_paths.append(str(i))
+
+    with open(i, "r") as f:
+        game_titles.append(json.loads(f.read()).get("title"))
 
 
 # show the user the available titles and have them select one
@@ -22,7 +22,7 @@ for id, game in enumerate(game_titles):
 whichGame = int(input("which game number would you like to play?\n"))
 
 # load the contents of the selected game file, or a random file if there's user error
-if whichGame in game_titles:
+if whichGame < len(game_titles):
     print("Nice choice!\n")
     with open(str(game_paths[whichGame]), "r") as f:
         data_loaded = json.loads(f.read())
@@ -32,27 +32,32 @@ else:
         data_loaded = json.loads(f.read())
 
 description = data_loaded.get("desc", "something interesting")
-questions = data_loaded.get("inputQuestions", {})
-randomCapital = chr(random.randint(65, 90))
-results = []
-
 print("\nOkay! Let's find out %s\nbased on your answers to a few simple questions...\n" % (description))
 
-for question in questions.keys():
-    currentGameData = questions[question]
-    answer = input(question + "\n")
+randomCapital = chr(random.randint(65, 90))
+results = []
+questions = data_loaded.get("inputQuestions")
 
-    # for the "character" type, find the result from the first letter of the given answer
-    # if there's user error, find the result from a random character
-    if currentGameData.get("_type") == "character":
-        results.append(currentGameData.get(answer.upper()[0],
-                                           currentGameData.get(randomCapital)))
+for entry in questions:
+    q = entry.get("question")
+    result = entry.get("result")
+
+    # Collect the user's input.
+    answer = input(q + "\n")
+
+    # For a question that expects a "character" result type,
+    # use only the first letter of the given answer to build the name.
+    # If there's user error, find the result from a random character
+    if entry.get("type") == "character":
+        c = answer.upper()[0]
+        results.append(result.get(c,
+                                  result.get(randomCapital)))
 
     # for all other types, try to find the result from the exact answer given
     # if the answer given doesn't exist, choose a random result
     else:
-        results.append(currentGameData.get(answer, currentGameData.get(
-            random.choice(list(currentGameData)[1:]))))
+        results.append(result.get(answer, result.get(
+            random.choice(list(result)))))
 
-#print the result for the user
+# print the result for the user
 print(data_loaded.get("output") + " ".join(results))
